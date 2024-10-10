a2de0cd23a5478e4ac79ca11388163c95b6b043d
Terrance Niechciol <TNiechciol@gmail.com>
2024-10-10 11:35:58 -0700

    Add alternative nvm location + more recent default node version

--- .config/zsh/config.d/nvm.zsh
+++ .config/zsh/config.d/nvm.zsh
@@ -1,4 +1,9 @@
 if [ -f $HOME/.nvm/nvm.sh ]; then
 	source $HOME/.nvm/nvm.sh --no-use ';' nvm $argv
-	export PATH="$PATH:$HOME/.nvm/versions/node/v16.14.0/bin/"
+	export PATH="$PATH:$HOME/.nvm/versions/node/v20.18.0/bin/"
+fi
+
+if [ -f /usr/share/nvm/init-nvm.sh ]; then
+	source /usr/share/nvm/init-nvm.sh
+	export PATH="$PATH:$HOME/.nvm/versions/node/v20.18.0/bin/"
 fi
